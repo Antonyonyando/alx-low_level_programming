00018531ee6f3afe8a720eae0aa6acf3dcf9ea70
Antonyonyando <onyandoantony4@gmail.com>
2023-06-15 12:39:18 -0700

    4c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ *main - the main point of entry
+ *return : must be 0 (success)
+ */
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
