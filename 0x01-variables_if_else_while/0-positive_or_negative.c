eb228c604df748b5e87bf99b9e3b69cc6fee1095
Antonyonyando <onyandoantony4@gmail.com>
2023-06-16 08:23:19 -0700

    c1

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	if (n == 0)
+	{/* if condition is true then print the following */
+	printf("The value of n is equal to 0");
+	}
+	else if (n > 0)
+	{ /* if else if condition is true print the following */
+	printf("The value of n is positive");
+	}
+	else
+	{/* if none of the condition is true */
+	printf("The value of n is negative");
+	}
+
+	printf("The exact value of n is: %d\n", n);
+	return (0);
+}
