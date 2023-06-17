ecdad03ed43c6a09c6f75f5b13beac0f8ab9ada9
Antonyonyando <onyandoantony4@gmail.com>
2023-06-16 20:55:44 -0700

    3c

---
+++ 0x01-variables_if_else_while/u.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char start = 'aA';
+	char ends = 'zZ''
+
+	while (start <= ends)
+	{
+		putchar(start);
+		start++;
+	}
+	putchar('\n')
+	return (0);
+}	
