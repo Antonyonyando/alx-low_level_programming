bf3287d912a32f5bf6aa15f231760acaa3750e52
Antonyonyando <onyandoantony4@gmail.com>
2023-06-20 15:55:50 -0700

    1c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include "main.h"
 /**
- * main - Point of entry
- * print_alphabet: prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  * Return: Always 0 (Succes)
  */
 
 void print_alphabet(void)
-/**
- * print_alphabet - Prints alphabet in lowercase
- */
 {
 	char n = 'a';
 
