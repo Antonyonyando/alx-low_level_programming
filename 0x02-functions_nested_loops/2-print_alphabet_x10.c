b9b0a5de097de0545f59cf3538706aeafd947f33
Antonyonyando <onyandoantony4@gmail.com>
2023-06-20 16:14:57 -0700

    c3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,6 +5,8 @@
  */
 void print_alphabet_x10(void)
 {
+	int count = 0;
+
 	char n = 'a';
 
 	while (n <= 'z')
@@ -12,6 +14,7 @@ void print_alphabet_x10(void)
 	_putchar(n);
 	n++;
 	}
+	while(count < 10);
 
 	_putchar('\n');
 }
