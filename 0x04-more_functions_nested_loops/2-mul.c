1a71ef1c8fd0d8b72839215a3c02bceb8084e9ad
Antonyonyando <onyandoantony4@gmail.com>
2023-06-22 15:59:55 -0700

    2nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - looks for multiplication of two digits
+ * @a: one of the integer to be multiplied
+ * @b: one of the integer to be multipiled
+ * Return: The results of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	printf("The multiplication of %d and %d is: %d\n", a, b, result);
+	return (0);
+}
