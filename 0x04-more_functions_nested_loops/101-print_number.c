c1028e78d2d6f4fa787b9ef5dfdccc740dba0c5a
Antonyonyando <onyandoantony4@gmail.com>
2023-06-22 19:10:05 -0700

    7c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_number - Prints integers
+ * @n: Character on check
+ * Return: 0 (Success)
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+	_putchar('-');
+	}
+
+}
