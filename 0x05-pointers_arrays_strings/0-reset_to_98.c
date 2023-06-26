34d518c034f9fd3428b3dd44e9098338d95d214d
Antonyonyando <onyandoantony4@gmail.com>
2023-06-26 05:28:58 -0700

    1pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - The function that shows int is a pointer
+ * @n: Value to be updated
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
