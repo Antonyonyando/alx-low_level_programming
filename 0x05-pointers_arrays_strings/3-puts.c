711c96d8096b6014842a34078e506aaaf22be44c
Antonyonyando <onyandoantony4@gmail.com>
2023-06-26 07:20:04 -0700

    3pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: Value to be checked
+ * Return: Always 0 (Success0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+	i++;
+	}
+
+	_putchar('\n');
+}
