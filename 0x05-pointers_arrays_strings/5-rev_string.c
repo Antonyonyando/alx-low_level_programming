109c34210ed23a07cac18748d71aba253deaa67d
Antonyonyando <onyandoantony4@gmail.com>
2023-06-26 10:25:29 -0700

    5pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: Value to be checked
+ * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s)
+{
+	while (*s != '\0')
+	{
+	_putchar(*s--);
+	}
+	_putchar('\n');
+}
