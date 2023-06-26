8050aa153652aa9f8b5f32fbe02c8a2742a56344
Antonyonyando <onyandoantony4@gmail.com>
2023-06-26 14:03:30 -0700

    8pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of astring
+ * @str: Value to be printed
+ * return: Always 0 (Success)
+ */
+
+void puts_half(char *str)
+{
+	int o = 0;
+
+	while (str[o] != '\0')
+	{
+	o++;
+	}
+	_putchar('\n');
+}
